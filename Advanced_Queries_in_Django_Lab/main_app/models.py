4740bb3837f7af78557a3d77382645ee6ef67773
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-12-05 08:52:24 +0100

    Advanced_Queries_in_Django_Lab

---
+++ Advanced_Queries_in_Django_Lab/main_app/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class ProductManager(models.Manager):
+    def available_products(self):
+        return self.filter(is_available=True)
+
+    def available_products_in_category(self, category_name):
+        return self.filter(category__name=category_name, is_available=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    is_available = models.BooleanField(default=True)
+    objects= ProductManager()
+
+    def __str__(self):
+        return f"{self.category.name}: {self.name}"
+
+
+class Customer(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='OrderProduct')
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
